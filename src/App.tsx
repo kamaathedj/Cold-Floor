39cc8372d2024834ab364770ffc64edbc24bc43d
kamaathedj <kamaudavikiruku@gmail.com>
2023-07-19 17:25:04 +0300

    add readme.md

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,10 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button type="submit" onClick={() => setCount((c) => c + 1)}>
-          count is
-          {' '}
-          {count}
+          count is {count}
         </button>
         <p>
-          Edit
-          {' '}
-          <code>src/App.tsx</code>
-          {' '}
-          and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
